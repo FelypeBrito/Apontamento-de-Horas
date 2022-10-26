f7b161d159072ae246e06c5d703e546b52d83b6c
Felype Brito <felype_britorocha@hotmail.com>
2022-10-26 13:53:53 -0300

    26/10

--- apontamentodehoras/webapp/controller/App.controller.js
+++ apontamentodehoras/webapp/controller/App.controller.js
@@ -1,6 +1,8 @@
 var sdata;
 var sHinicial;
 var sHfinal;
+var sHtotal;
+
 sap.ui.define([
     "./BaseController",
     "sap/ui/model/json/JSONModel",
@@ -9,25 +11,27 @@ sap.ui.define([
     "sap/m/MessageToast",
     "sap/ui/core/UIComponent",
     "sap/ui/core/Core",
-	"sap/ui/core/library",
-	"sap/ui/unified/library",
-	"sap/ui/unified/DateTypeRange"
-], function (BaseController,JSONModel,formatter,History,  MessageToast, UIComponent, Core,CoreLibrary, UnifiedLibrary, DateTypeRange) {
+    "sap/ui/core/library",
+    "sap/ui/unified/library",
+    "sap/ui/unified/DateTypeRange",
+    "sap/ui/core/format/DateFormat"
+], function (BaseController, JSONModel, formatter, History, MessageToast, UIComponent, Core, CoreLibrary, UnifiedLibrary, DateTypeRange, DateFormat) {
     "use strict";
 
     var CalendarDayType = UnifiedLibrary.CalendarDayType,
-		ValueState = CoreLibrary.ValueState;
+        ValueState = CoreLibrary.ValueState;
+
 
 
     return BaseController.extend("apontamento.apontamentodehoras.controller.App", {
 
-    formatter: formatter,
+        formatter: formatter,
 
         onInit: function () {
             // apply content density mode to root view
             this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
-                // create model
-              
+            // create model
+
         },
 
         onCancelar: function (oEvent) {
@@ -35,24 +39,30 @@ sap.ui.define([
         },
         onGravar: function () {
             var oModel = this.getModel();
+
+            sHtotal = this.duracao(sHinicial, '00:00', '00:00', sHfinal);
+
+
             var dados = {
+                Clinid: this.byId("inpCliente").getValue(),
                 Funcid: this.byId("inpFuncid").getValue(),
                 Data: sdata,
                 Atividade: this.byId("inpAtividade").getValue(),
                 Hentrada: sHinicial,
                 Hsaida: sHfinal,
-                Descricao: this.byId("inpDescricao").getValue()  
+                Descricao: this.byId("inpDescricao").getValue(),
+                Htotal: sHtotal,
             };
-            
+
             oModel.create("/HorasTrabalhadasSet", dados, {
                 success: function (oDados, resposta) {
                     MessageToast.show('Horas apontadas com sucesso!!.');
                     var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-                    oRouter.navTo("funcionarios", {});
-                    
+                    oRouter.navTo("worklist", {
+                        objectId: oDados
+                    }, true);
                 }.bind(this),
                 error: function (oError) {
-                console.log(e)
                     debugger
                 }.bind(this),
             });
@@ -73,11 +83,34 @@ sap.ui.define([
         handleChange3: function (oEvent) {
             var oTP = oEvent.getSource(),
                 sValue = oEvent.getParameter("value");
+
             sHfinal = sValue;
         },
 
-        
-        
+        parse: function (horario) {
+            // divide a string em duas partes, separado por dois-pontos, e transforma em número
+            let [hora, minuto] = horario.split(':').map(v => parseInt(v));
+            if (!minuto) { // para o caso de não ter os minutos
+                minuto = 0;
+            }
+            return minuto + (hora * 60);
+        },
+
+        // calcula a duração total em minutos
+        duracao: function (entrada1, saida1, entrada2, saida2) {
+            let periodo = (this.parse(saida1) - this.parse(entrada1)) + (this.parse(saida2) - this.parse(entrada2));
+            let horas = Math.trunc(periodo / 60).toString()
+            let minutos = (periodo % 60).toString();
+            horas = ("00" + horas).slice(-2);
+            minutos = ("00" + minutos).slice(-2);
+            return (horas + ":" + minutos);
+        },
+
+
+
+
+
+
 
     });
 },
--- apontamentodehoras/webapp/controller/BaseController.js
+++ apontamentodehoras/webapp/controller/BaseController.js
@@ -1,8 +1,106 @@
+String.prototype.pad = function (size) {
+	var s = this;
+	while (s.length < (size || 2)) { s = "0" + s; }
+	return s;
+}
+
+/*
+ * Triggered when date needs to be added
+*/
+Date.prototype.addDays = function (days) {
+	var date = new Date(this.valueOf());
+	date.setDate(date.getDate() + days);
+	return date;
+}
+
+/*
+ * Triggered when a date needs to be subtracted
+*/
+Date.prototype.removeDays = function (days) {
+	var date = new Date(this.valueOf());
+	date.setDate(date.getDate() - days);
+	return date;
+}
+
+Date.prototype.removeHoras = function () {
+	var date = new Date(this.valueOf());
+	var dateBR = date.formatDataBR();
+	return new Date().parseDataBR(dateBR);
+}
+
+/*
+ * BR date formatter
+*/
+Date.prototype.formatDataBR = function () {
+	var date = new Date(this.valueOf());
+	return ("00" + date.getDate()).slice(-2) + '.' + ("00" + (date.getMonth() + 1)).slice(-2) + '.' + date.getFullYear();
+}
+
+Date.prototype.parseDataBR = function (dataBR) {
+	var dia = parseInt(dataBR.substring(0, 2));
+	var mes = parseInt(dataBR.substring(3, 5));
+	var ano = parseInt(dataBR.substring(6, 10));
+	return new Date(ano, mes - 1, dia);
+}
+
+Date.prototype.formatData = function () {
+	var date = new Date(this.valueOf() + this.getTimezoneOffset() * 60000);
+
+	var ano = date.getFullYear();
+	var mes = date.getMonth() + 1;
+	mes = ("00" + mes).slice(-2);
+	var dia = date.getDate();
+	dia = ("00" + dia).slice(-2);
+
+	return dia + '.' + mes + '.' + ano;
+}
+
+Date.prototype.menorHoje = function () {
+	var hoje = new Date();
+	var dataHoje = new Date(hoje.getFullYear(), hoje.getMonth(), hoje.getDate());
+
+	var timeDiff = this.valueOf() - dataHoje.getTime();
+	var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+	return (diffDays < 0);
+}
+
+Date.prototype.maiorHoje = function () {
+	var hoje = new Date();
+	var dataHoje = new Date(hoje.getFullYear(), hoje.getMonth(), hoje.getDate());
+
+	var timeDiff = this.valueOf() - dataHoje.getTime();
+	var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+	return (diffDays > 0);
+}
+
+Date.prototype.menorDataLimite = function (days) {
+	var limite = new Date().addDays(days);
+	var dataLimite = new Date(limite.getFullYear(), limite.getMonth(), limite.getDate());
+
+	var timeDiff = this.valueOf() - dataLimite.getTime();
+	var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+	return (diffDays < 0);
+}
+
+Date.prototype.dataValida = function (date) {
+	var matches = /(\d{2})[-.\/](\d{2})[-.\/](\d{4})/.exec(date);
+	if (matches == null) {
+		return false;
+	}
+	var dia = matches[1];
+	var mes = matches[2] - 1;
+	var ano = matches[3];
+	var data = new Date(ano, mes, dia);
+	return data.getDate() == dia && data.getMonth() == mes && data.getFullYear() == ano;
+}
+
+
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/UIComponent",
-    "sap/m/library"
-], function (Controller, UIComponent, mobileLibrary) {
+    "sap/m/library",
+    "sap/ui/core/format/DateFormat"
+], function (Controller, UIComponent, mobileLibrary, DateFormat) {
     "use strict";
 
     // shortcut for sap.m.URLHelper
@@ -17,6 +115,18 @@ sap.ui.define([
         getRouter : function () {
             return UIComponent.getRouterFor(this);
         },
+
+        formatDate: (date)=> (date  != undefined && date !== "") ? date.formatData(): '',
+
+		formatHour: function (date) {
+			if (date != undefined) {
+				var timeFormat = DateFormat.getTimeInstance({ pattern: "HH:mm:ss " });
+				var TZOffsetMs = new Date(0).getTimezoneOffset() * 60 * 1000;
+				return timeFormat.format(new Date(date.ms + TZOffsetMs));  //11:00 AM
+			} else {
+				return '';
+			}
+		},
 
         /**
          * Convenience method for getting the view model by name.
