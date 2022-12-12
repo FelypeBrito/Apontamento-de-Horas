bf6a070afd6cb49d89a14083c6dbb88affa1e825
Felype Brito <felype_britorocha@hotmail.com>
2022-12-12 16:20:56 -0300

    12/12

--- apontamentodehoras/webapp/controller/NewProject.controller.js
+++ apontamentodehoras/webapp/controller/NewProject.controller.js
@@ -1,18 +1,12 @@
-
-
 sap.ui.define([
     "./BaseController",
     "sap/ui/model/json/JSONModel",
     "../model/formatter",
-    "sap/ui/core/routing/History",
     "sap/m/MessageToast",
-    "sap/ui/core/UIComponent",
-    "sap/ui/core/Core",
-    "sap/ui/core/library",
-    "sap/ui/unified/library",
-    "sap/ui/unified/DateTypeRange",
-    "sap/ui/core/format/DateFormat"
-], function (BaseController, JSONModel, formatter, History, MessageToast, UIComponent, Core, CoreLibrary, UnifiedLibrary, DateTypeRange, DateFormat) {
+    "sap/ui/core/Fragment"
+
+   
+], function (BaseController, JSONModel, formatter, MessageToast, Fragment) {
     "use strict";
 
 
@@ -23,6 +17,7 @@ sap.ui.define([
         onInit: function () {
             // apply content density mode to root view
             this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+          
             // create model
 
         },
@@ -56,7 +51,89 @@ sap.ui.define([
         },
 
 
-    });
+        /*valueHelpRequest: function (oEvent) {
+            var oValueHelpDialog = new sap.ui.ux3.ToolPopup({
+                modal: true,
+                inverted: false,                          // disable color inversion
+                title: "Select Material Number",
+                opener: "idMatnrInput",             // locate dialog next to this field
+                closed: function (oEvent) {
+                }
+            });
+            var oOkButton = new sap.ui.commons.Button({
+                text: "OK",
+                press: function (oEvent) {
+                    oEvent.getSource().getParent().close();
+                }
+            });
+
+            oValueHelpDialog.addButton(oOkButton);
+
+            oValueHelpDialog.open();
+        },*/
+
+        
+
+       /* onValueHelpRequest: function () {
+			if (!this._oValueHelpDialog) {
+				Fragment.load({
+					name: "apontamento.apontamentodehoras.ValueHelpDialog",
+					controller: this
+				}).then(function (oValueHelpDialog){
+					this._oValueHelpDialog = oValueHelpDialog;
+					this.getView().addDependent(this._oValueHelpDialog);
+					this._configValueHelpDialog();
+					this._oValueHelpDialog.open();
+				}.bind(this));
+			} else {
+				this._configValueHelpDialog();
+				this._oValueHelpDialog.open();
+			}
+		},
+
+		_configValueHelpDialog: function () {
+			var sInputValue = this.byId("productInput").getValue(),
+				oModel = this.getView().getModel();
+		
+
+                oModel.read("/Cliente1Set", {
+                    //method: "GET",
+                    success: function (oDados, resposta) {
+                        var aProducts = oModel.getProperty("/Cliente1Set");
+                        oModel.setData(oData.results);
+		   	        oView.setModel(oModel, "info");
+    
+                    }.bind(this),
+    
+                    error: function (oError) {
+                        var erro;
+                        erro = JSON.parse(oError.responseText);
+                        MessageToast.show("Dados incorreto ou usuário não cadastrado!");
+                    }.bind(this),
+                });
+
+			aProducts.forEach(function (oProduct) {
+				oProduct.selected = (oProduct.Nome === sInputValue);
+			});
+			oModel.setProperty("/Cliente1Set", aProducts);
+		},
+
+		onValueHelpDialogClose: function (oEvent) {
+			var oSelectedItem = oEvent.getParameter("selectedItem"),
+				oInput = this.byId("productInput");
+
+			if (!oSelectedItem) {
+				oInput.resetProperty("value");
+				return;
+			}
+
+			oInput.setValue(oSelectedItem.getTitle());
+		}*/
+
+	});
+
+
+
 },
 
 );
